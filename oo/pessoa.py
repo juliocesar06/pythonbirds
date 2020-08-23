d116aeb3b2a7f7b1f3745b599580cf3018fd6644
juliocesar06 <julio06cesar11@gmail.com>
2020-08-23 00:25:56 -0300

    criado atributo de classe

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,4 +1,7 @@
 class Pessoa:
+    # atribudo defaul ou atributo de classe
+    olhos = 2
+
     # metodo construtor da classe
     def __init__(self,*filhos, nome, idade=28):# atributos de classe
         self.idade = idade
@@ -36,3 +39,19 @@ if __name__ == '__main__':
     # acessando tdos os atributos dos objetos
     print(julio.__dict__)
     print(renzo.__dict__)
+
+    # acessando atributo de classe(ele e acesando pela classe)
+    print(Pessoa.olhos)
+
+    # acessando o atributo de classe(pelo objeto)
+    print(julio.olhos)
+    print(renzo.olhos)
+
+    # ID da classe e do objeto e o mesmo idenpedente de onde ele e acessado
+    print(id(Pessoa.olhos),id(julio.olhos),id(renzo.olhos))# mesmo valor de id(pois esta sendo instanciado  na classe)
+
+    # ao mudar o atributo de classe usdabndo um atributo dinamico vc altera a id do objeto em que esta sendo instanciado.
+    renzo.olhos = 1
+
+    # ID da classe e do objeto e o mesmo idenpedente de onde ele e acessado
+    print(id(Pessoa.olhos),id(julio.olhos),id(renzo.olhos)) # id de renzo mudou pq foi "instaciado dinamicamente"
