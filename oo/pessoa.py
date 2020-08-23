8ec54f87bee0bb7c9fa71d7a28a5e56f735eefbe
juliocesar06 <julio06cesar11@gmail.com>
2020-08-23 00:04:49 -0300

    criado atributo dinamico

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,14 +1,38 @@
 class Pessoa:
-    def __init__(self,*filhos, nome, idade=28):
+    # metodo construtor da classe
+    def __init__(self,*filhos, nome, idade=28):# atributos de classe
         self.idade = idade
         self.nome = nome
         self.filhos = list(filhos)
+
+    # metodo de classe
     def cumprimentar(self):
         return f'Ola mundo!!!{id(self)}'
 
+# metodo main ->> qdo importar esse pagina apenas execultara od metodos.
 if __name__ == '__main__':
+
+    # instanciando as pessoas
     renzo = Pessoa(nome='Renzo')
     julio = Pessoa(renzo,nome='Julio')
+
     print(julio.filhos)
+
     for filho in julio.filhos:
         print(filho.nome)
+        print(id(filho))
+
+    # atribuinado dinamicamente um atributo no objeto julio(apenas nesse objeto)
+    julio.sobrenome = 'Cesar'
+    print(julio.sobrenome)
+
+    # acessando tdos os atributos dos objetos
+    print(julio.__dict__)
+    print(renzo.__dict__)
+
+    # removendo algum atributo dinamicamente pelo comando del
+    del renzo.idade
+
+    # acessando tdos os atributos dos objetos
+    print(julio.__dict__)
+    print(renzo.__dict__)
