432ad897b1ef6d36645a001f3f78c8cd595abcb6
juliocesar06 <julio06cesar11@gmail.com>
2020-08-22 23:45:49 -0300

    criado atributo filhos

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,17 +1,14 @@
 class Pessoa:
-    def __init__(self,nome,idade,time):
-        self.time = time
+    def __init__(self,*filhos, nome, idade=28):
         self.idade = idade
         self.nome = nome
-
+        self.filhos = list(filhos)
     def cumprimentar(self):
         return f'Ola mundo!!!{id(self)}'
 
 if __name__ == '__main__':
-    p = Pessoa('King',28,'Cruzeiro')
-    print(Pessoa .cumprimentar(p))
-    print(id(p))
-    print(p.cumprimentar())
-    print(p.nome)
-    p.nome = 'Julio'
-    print(p.nome,p.idade,p.time)
+    renzo = Pessoa(nome='Renzo')
+    julio = Pessoa(renzo,nome='Julio')
+    print(julio.filhos)
+    for filho in julio.filhos:
+        print(filho.nome)
