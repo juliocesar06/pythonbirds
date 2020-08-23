fbf2feeb7234f4e1bf81e58b56e8012c21180016
juliocesar06 <julio06cesar11@gmail.com>
2020-08-22 23:33:25 -0300

    criado atributos de classe (nome,idade,time)

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,9 +1,17 @@
 class Pessoa:
+    def __init__(self,nome,idade,time):
+        self.time = time
+        self.idade = idade
+        self.nome = nome
+
     def cumprimentar(self):
         return f'Ola mundo!!!{id(self)}'
 
 if __name__ == '__main__':
-    p = Pessoa()
+    p = Pessoa('King',28,'Cruzeiro')
     print(Pessoa .cumprimentar(p))
     print(id(p))
     print(p.cumprimentar())
+    print(p.nome)
+    p.nome = 'Julio'
+    print(p.nome,p.idade,p.time)
