778b6cfd225aa5befe2d9990409e85487f93da9e
juliocesar06 <julio06cesar11@gmail.com>
2020-08-25 00:08:03 -0300

    criando um carro

--- oo/carro.py
+++ oo/carro.py
@@ -1,38 +1,137 @@
+"""
+#teste
+>>> motor = Motor()
+>>> motor.velocidade
+0
+>>> motor.acelerar()
+>>> motor.velocidade
+1
+>>> motor.acelerar()
+>>> motor.velocidade
+2
+>>> motor.acelerar()
+>>> motor.velocidade
+3
+>>> motor.frear()
+>>> motor.velocidade
+1
+>>> motor.frear()
+>>> motor.velocidade
+0
+>>> direcao = Direcao()
+>>> direcao.valor
+'Norte'
+>>> direcao.girar_dir()
+>>> direcao.valor
+'Leste'
+>>> direcao.girar_dir()
+>>> direcao.valor
+'Sul'
+>>> direcao.girar_dir()
+>>> direcao.valor
+'Oeste'
+>>> direcao.girar_dir()
+>>> direcao.valor
+'Norte'
+>>> direcao.girar_esq()
+>>> direcao.valor
+'Oeste'
+>>> direcao.girar_esq()
+>>> direcao.valor
+'Sul'
+>>> direcao.girar_esq()
+>>> direcao.valor
+'Leste'
+>>> direcao.girar_esq()
+>>> direcao.valor
+'Norte'
+>>> carro = Carro(Direcao,Motor)
+>>> carro.calcular_velocidade()
+0
+>>> carro.acelerar()
+>>> carro.calcular_velocidade()
+1
+>>> carro.acelerar()
+>>> carro.calcular_velocidade()
+2
+>>> carro.acelerar()
+>>> carro.calcular_velocidade()
+3
+>>> carro.frear()
+>>> carro.calcular_velocidade()
+1
+>>> carro.frear()
+>>> carro.calcular_velocidade()
+0
 
-class Carro:
-    # atribuiçao da classe velocidade
-    velocidade = 0
+"""
+
+
+class Motor:
 
     def __init__(self):
-        pass
+        self.velocidade = 0
 
-class Motor(Carro):
-
-
-    # adciciona 1 ponto na velocidade
     def acelerar(self):
-        Carro.velocidade += 1
+        self.velocidade += 1
 
-    # funçao retira 2 pontos de velocidade, se velocidade for <=1 ,entao velocidade = 0
     def frear(self):
-
-        if Carro.velocidade <=1:
-            Carro.velocidade = 0
+        if self.velocidade <= 1:
+            self.velocidade = 0
         else:
-            Carro.velocidade -= 2
+            self.velocidade -= 2
+
+
+class Direcao:
+
+    def __init__(self):
+        self.mov = ['Norte', 'Leste', 'Sul', 'Oeste']
+        self.valor = self.mov[0]
+
+    def girar_dir(self):
+        if self.valor == self.mov[0]:
+            self.valor = self.mov[1]
+        elif self.valor == self.mov[1]:
+            self.valor = self.mov[2]
+        elif self.valor == self.mov[2]:
+            self.valor = self.mov[3]
+        else:
+            self.valor = self.mov[0]
+
+    def girar_esq(self):
+        if self.valor == self.mov[3]:
+            self.valor = self.mov[2]
+        elif self.valor == self.mov[2]:
+            self.valor = self.mov[1]
+        elif self.valor == self.mov[1]:
+            self.valor = self.mov[0]
+        else:
+            self.valor = self.mov[3]
+
+
+class Carro:
+    velocidade = 0
+    def __init__(self,direcao,motor):
+        self.direcao = direcao
+        self.motor = motor
 
 
 
-motor = Motor()
-print(motor.velocidade)#0
-motor.acelerar()
-print(motor.velocidade)#1
-motor.acelerar()
-print(motor.velocidade)#2
-motor.acelerar()
-print(motor.velocidade)#3
-motor.frear()
-print(motor.velocidade)#1
-motor.frear()
-print(motor.velocidade)#0
 
+    def calcular_velocidade(self):
+        return self.velocidade
+
+    def acelerar(self):
+        self.velocidade += 1
+
+    def frear(self):
+        if self.velocidade <= 1:
+            self.velocidade = 0
+        else:
+            self.velocidade -= 2
+
+
+
+carro = Carro('Norte',0)
+carro.acelerar()
+carro.frear()
