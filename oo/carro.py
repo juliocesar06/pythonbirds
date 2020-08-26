a069c454a4df6b7d8d5b81fb58281ed0291e8ec6
juliocesar06 <julio06cesar11@gmail.com>
2020-08-25 23:09:19 -0300

    oncrementando o carro e finalizando o programa

--- oo/carro.py
+++ oo/carro.py
@@ -65,33 +65,58 @@
 0
 
 """
+# classe motor
 class Motor:
+
+    # velocidade inicial == 0
     def __init__(self):
         self.velocidade = 0
 
+    # acrescenta em 1 a velocidade
     def acelerar(self):
         self.velocidade += 1
 
+    # frear reduz a velocidade em 2,mas o menor valor possivel é 0
     def frear(self):
         self.velocidade -= 2
-        self.velocidade = max(0,self.velocidade)
+        self.velocidade = max(0,self.velocidade) #funçao MAX(0,self.vel) 0 e o maximo q pode chegar
+
+# criando constante comforme a pep sempre sendo td maiúscula
 NORTE ='Norte'
 SUL = 'Sul'
 LESTE = 'Leste'
 OESTE = 'Oeste'
 
+# classe Direção
 class Direcao:
 
-    rotacao_direita_dic = {NORTE:LESTE ,LESTE:SUL ,SUL:OESTE ,OESTE:NORTE}
-    rotacao_esquerda_dic = {NORTE:OESTE ,OESTE:SUL ,SUL:LESTE ,LESTE:NORTE}
+    # dicionario com rotaçao dir e esq
+    rotacao_direita_dic = {NORTE:LESTE ,LESTE:SUL ,SUL:OESTE,OESTE:NORTE}
+    rotacao_esquerda_dic = {NORTE:OESTE ,OESTE:SUL,SUL:LESTE, LESTE:NORTE }
 
     def __init__(self):
         self.valor = NORTE
 
     def girar_dir(self):
-      self.rotacao_direita_dic[self.valor]
+      self.valor = self.rotacao_direita_dic[self.valor]
     def girar_esq(self):
-       self.rotacao_esquerda_dic[self.valor]
+       self.valor =self.rotacao_esquerda_dic[self.valor]
 
+class Carro:
+
+    def __init__(self,direcao,motor):
+        self.velocidade = 0
+
+    def calcular_velocidade(self):
+        return self.velocidade
+
+    def acelerar(self):
+        self.velocidade += 1
+
+    def frear(self):
+        if self.velocidade <=1:
+            self.velocidade = 0
+        else:
+            self.velocidade -= 2
 
 
